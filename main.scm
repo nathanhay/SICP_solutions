3cd217cd2aa121a8810d3f424bd97746f8ba9365
Nathan Hay <nathan.a.hay@vanderbilt.edu>
2021-07-25 22:21:58 +0000

    SICP 1.11

--- main.scm
+++ main.scm
@@ -1,3 +1,6 @@
+; Solution to exercise 1.11 from Structure and Interpretation  of Computer Programs
+; "A function f is defined by the rule that f(n) = n if n<3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n> 3. Write a procedure that computes f by means of a recursive process. Write a procedure that computes f by means of an iterative process.'
+
 (define (f-recursive n)
   (cond ((< n 3) n)
         (else (+ (f-recursive (- n 1)) (* 2 (f-recursive (- n 2))) (* 3 (f-recursive (- n 3)))))))
@@ -5,8 +8,8 @@
 (define (f n)
   (define (f-iter a b c i)
     (if (= i 3)
-        (+ a (* 2 b) (* 3 c))
-        (f-iter (+ a (* 2 b) (* 3 c)) a b (- i 1))))
+        (+ a (* 2 b) (* 3 c)) ; base case
+        (f-iter (+ a (* 2 b) (* 3 c)) a b (- i 1)))) ; inductive step
   (if (< n 3)
       n
       (f-iter 2 1 0 n)))
